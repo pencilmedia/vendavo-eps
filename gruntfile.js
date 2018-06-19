82fd8d9b420a8a4377f7b017b6f02c24ba96c6a3
ralph <ralph@pencilmedia.com>
2018-06-19 01:33:45 -0700

    xxx

--- gruntfile.js
+++ gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-sass');
   require('load-grunt-tasks')(grunt);
 
   var optionsContext = grunt.option('context') || '';
